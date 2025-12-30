ce51c4b92803f8e120378b9d85d8ac479eb1e305
asa <asa@anime.digital>
2025-12-30 23:55:45 +0000

    refactor: Move the 'show' route definition within the 'anime' route map.

--- assets/javascripts/discourse/route-map.js
+++ assets/javascripts/discourse/route-map.js
@@ -1,8 +1,8 @@
 export default function () {
     this.route("anime", { path: "/anime" }, function () {
-        this.route("show", { path: "/:id" });
         this.route("watchlist", { path: "/watchlist" });
         this.route("seasons", { path: "/seasons(/:year/:season)" });
+        this.route("show", { path: "/:id" });
     });
     this.route("user", { path: "/u/:username", resetNamespace: true }, function () {
         this.route("watchlist");
