70306fed17b2ba94d4d0cc052608cb4447d635a9
asa <asa@anime.digital>
2025-12-30 15:41:19 +0000

    feat: allow composer to be opened with a pre-set anime MAL ID

--- assets/javascripts/discourse/initializers/anime-database.js
+++ assets/javascripts/discourse/initializers/anime-database.js
@@ -5,6 +5,17 @@ export default {
     initialize() {
         withPluginApi("1.34.0", (api) => {
             api.serializeOnCreate("anime_mal_id");
+
+            api.modifyClass("model:composer", {
+                pluginId: "anime-plugin-ex",
+
+                open(opts) {
+                    if (opts.anime_mal_id) {
+                        this.set("anime_mal_id", opts.anime_mal_id);
+                    }
+                    return this._super(...arguments);
+                }
+            });
         });
     },
 };
