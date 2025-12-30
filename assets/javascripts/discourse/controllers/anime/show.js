a26f5a83dbcc17b1615dfa2a8e9f7925bb4ea21c
asa <asa@anime.digital>
2025-12-30 01:34:33 +0000

    feat: Refactor composer topic creation to use `topicTitle`, `topicCategoryId`, and `topicBody` with category ID validation.

--- assets/javascripts/discourse/controllers/anime/show.js
+++ assets/javascripts/discourse/controllers/anime/show.js
@@ -4,16 +4,18 @@ import { service } from "@ember/service";
 
 export default class ShowController extends Controller {
     @service composer;
-    @service siteSettings;
+    @service("site-settings") siteSettings;
 
     @action
     createDiscussion() {
+        const categoryId = parseInt(this.siteSettings.anime_database_category, 10);
+
         this.composer.open({
             action: "createTopic",
-            title: `Discussion: ${this.model.title}`,
-            categoryId: this.siteSettings.anime_database_category,
+            topicTitle: `Discussion: ${this.model.title}`,
+            topicCategoryId: categoryId > 0 ? categoryId : null,
+            topicBody: `Discussing ${this.model.title}\n\n[quote]\n${this.model.synopsis}\n[/quote]`,
             topicCustomFields: { anime_mal_id: this.model.mal_id.toString() },
-            importQuote: `Discussing ${this.model.title}\n\n[quote]\n${this.model.synopsis}\n[/quote]`,
         });
     }
 }
