10f42bcf0a631406bf26c816894b81250164df94
asa <asa@anime.digital>
2026-01-01 22:12:44 +0000

    fix: Correct relative import path for `applyCalendarStyles`.

--- assets/javascripts/discourse/routes/anime/calendar.js
+++ assets/javascripts/discourse/routes/anime/calendar.js
@@ -1,6 +1,6 @@
 import Route from "@ember/routing/route";
 import { ajax } from "discourse/lib/ajax";
-import applyCalendarStyles from "../lib/anime-calendar-styles";
+import applyCalendarStyles from "../../lib/anime-calendar-styles";
 
 export default class CalendarRoute extends Route {
     model() {
