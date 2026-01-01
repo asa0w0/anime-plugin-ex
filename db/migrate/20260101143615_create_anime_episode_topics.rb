094799d7c5a2150e93d45fd9c8c364a4527eec1d
asa <asa@anime.digital>
2026-01-01 14:53:36 +0000

    feat: Create `anime_episode_topics` table with explicit up/down methods, column constraints, and renamed indexes.

--- db/migrate/20260101143615_create_anime_episode_topics.rb
+++ db/migrate/20260101143615_create_anime_episode_topics.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
 class CreateAnimeEpisodeTopics < ActiveRecord::Migration[7.0]
-  def change
+  def up
+    return if table_exists?(:anime_episode_topics)
+    
     create_table :anime_episode_topics do |t|
-      t.string :anime_id, null: false
+      t.string :anime_id, null: false, limit: 255
       t.integer :episode_number, null: false
       t.integer :topic_id, null: false
       t.datetime :aired_at
-      t.timestamps
+      t.timestamps null: false
     end
     
-    add_index :anime_episode_topics, [:anime_id, :episode_number], unique: true, name: 'index_anime_episode_topics_on_anime_and_episode'
-    add_index :anime_episode_topics, :topic_id
+    add_index :anime_episode_topics, [:anime_id, :episode_number], unique: true, name: 'idx_anime_ep_topics_anime_ep'
+    add_index :anime_episode_topics, :topic_id, name: 'idx_anime_ep_topics_topic_id'
+  end
+  
+  def down
+    drop_table :anime_episode_topics if table_exists?(:anime_episode_topics)
   end
 end
