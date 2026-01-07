271520471bf6a0bc8aea029c0003c9f5333c060f
asa <asa@anime.digital>
2026-01-07 00:42:03 +0000

    feat: Trigger anime search on Enter or blur, remove debouncing, and manage input state locally.

--- assets/javascripts/discourse/components/anime-search-bar.js
+++ assets/javascripts/discourse/components/anime-search-bar.js
@@ -3,48 +3,42 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class AnimeSearchBar extends Component {
-    @tracked timer = null;
+    @tracked localValue = "";
+
+    constructor() {
+        super(...arguments);
+        this.localValue = this.args.value || "";
+    }
 
     @action
     onInput(event) {
-        const value = event.target.value;
-        this.debounceSearch(value);
+        this.localValue = event.target.value;
     }
 
     @action
     onKeyPress(event) {
         if (event.key === "Enter") {
-            if (this.timer) {
-                clearTimeout(this.timer);
-            }
-            if (this.args.onChange) {
-                this.args.onChange(event.target.value);
-            }
+            this.triggerSearch();
         }
     }
 
-    debounceSearch(value) {
-        if (this.timer) {
-            clearTimeout(this.timer);
+    @action
+    onBlur() {
+        // Only search on blur if value changed
+        if (this.localValue !== this.args.value) {
+            this.triggerSearch();
         }
+    }
 
-        // Don't search for very short queries (except empty to clear)
-        if (value.length > 0 && value.length < 2) {
-            return;
+    triggerSearch() {
+        if (this.args.onChange) {
+            this.args.onChange(this.localValue);
         }
-
-        this.timer = setTimeout(() => {
-            if (this.args.onChange) {
-                this.args.onChange(value);
-            }
-        }, 800); // Increased from 500ms to 800ms for more typing time
     }
 
     @action
     clearSearch() {
-        if (this.timer) {
-            clearTimeout(this.timer);
-        }
+        this.localValue = "";
         if (this.args.onChange) {
             this.args.onChange("");
         }
