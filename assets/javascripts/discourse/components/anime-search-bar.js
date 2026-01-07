c26df7419a3467064544aeb631b150b1d605b6f6
asa <asa@anime.digital>
2026-01-07 00:35:46 +0000

    feat: introduce minimum search query length and increase debounce delay

--- assets/javascripts/discourse/components/anime-search-bar.js
+++ assets/javascripts/discourse/components/anime-search-bar.js
@@ -28,11 +28,16 @@ export default class AnimeSearchBar extends Component {
             clearTimeout(this.timer);
         }
 
+        // Don't search for very short queries (except empty to clear)
+        if (value.length > 0 && value.length < 2) {
+            return;
+        }
+
         this.timer = setTimeout(() => {
             if (this.args.onChange) {
                 this.args.onChange(value);
             }
-        }, 500);
+        }, 800); // Increased from 500ms to 800ms for more typing time
     }
 
     @action
