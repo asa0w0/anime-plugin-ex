7f5f317219b92b337aa462a9de5bffe4dd66e01e
asa <asa@anime.digital>
2026-01-10 20:54:38 +0000

    Fix Search

--- assets/javascripts/discourse/components/anime-search-bar.js
+++ assets/javascripts/discourse/components/anime-search-bar.js
@@ -3,42 +3,48 @@ import { action } from "@ember/object";
 import { tracked } from "@glimmer/tracking";
 
 export default class AnimeSearchBar extends Component {
-    @tracked localValue = "";
-
-    constructor() {
-        super(...arguments);
-        this.localValue = this.args.value || "";
-    }
+    @tracked timer = null;
 
     @action
     onInput(event) {
-        this.localValue = event.target.value;
+        const value = event.target.value;
+        this.debounceSearch(value);
     }
 
     @action
     onKeyPress(event) {
         if (event.key === "Enter") {
-            this.triggerSearch();
+            if (this.timer) {
+                clearTimeout(this.timer);
+            }
+            if (this.args.onChange) {
+                this.args.onChange(event.target.value);
+            }
         }
     }
 
-    @action
-    onBlur() {
-        // Only search on blur if value changed
-        if (this.localValue !== this.args.value) {
-            this.triggerSearch();
+    debounceSearch(value) {
+        if (this.timer) {
+            clearTimeout(this.timer);
         }
-    }
 
-    triggerSearch() {
-        if (this.args.onChange) {
-            this.args.onChange(this.localValue);
+        // Don't search for very short queries (except empty to clear)
+        if (value.length > 0 && value.length < 2) {
+            return;
         }
+
+        this.timer = setTimeout(() => {
+            if (this.args.onChange) {
+                this.args.onChange(value);
+            }
+        }, 1500); // 1.5 seconds - plenty of time to type
     }
 
     @action
     clearSearch() {
-        this.localValue = "";
+        if (this.timer) {
+            clearTimeout(this.timer);
+        }
         if (this.args.onChange) {
             this.args.onChange("");
         }
